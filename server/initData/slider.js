c5d3be1530fdbdb31ac8fa09e55f288cd052858e
“Liuherring” <liuherring333@gamil.com>
2022-11-17 14:19:20 +0800

    init

---
+++ server/initData/slider.js
@@ -0,0 +1,26 @@
+const sliderData = [{
+        img: "www.baidu.com/img/flexible/logo/pc/result.png",
+        sort: 1
+    },
+    {
+        img: "http://img.mukewang.com/5d1c5aec00011c1016000540.jpg",
+        sort: 2
+    },
+    {
+        img: "http://img.mukewang.com/5d108b1500010bfe18720632.jpg",
+        sort: 3
+    },
+    {
+        img: "http://img.mukewang.com/5d1466c5000172a516000540.jpg",
+        sort: 4
+    },
+    {
+        img: "http://img.mukewang.com/5cb833cf0001efb716000540.jpg",
+        sort: 5
+    },
+    {
+        img: "http://img.mukewang.com/5c0fd2630001ef2118720632.jpg",
+        sort: 6
+    }
+]
+export default sliderData
